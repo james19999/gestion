b6cc5a9fe5e20d0ae8d9ac5db2d1a553ac115f8a
james19999 <komlanahiakpor23@gmail.com>
2021-11-15 12:16:45 +0000

    créaction du model classe et factory

---
+++ .history/database/factories/ClasseFactory_20211115115032.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Classe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClasseFactory extends Factory
+{
+    protected $model=Classe::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ .history/database/factories/ClasseFactory_20211115115246.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Classe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClasseFactory extends Factory
+{
+    protected $model=Classe::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'libelle'=>$this->faker->word(),
+            'place'=>$this->faker->numberBetween(10,100),
+            'etage'=>" deuxième etage",
+        ];
+    }
+}
